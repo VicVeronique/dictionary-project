ff74db5a75a070028fc3f26eb774ad8870c08513
VicVeronique <107785778+VicVeronique@users.noreply.github.com>
2022-10-29 18:17:26 +0300

    phonetics added

--- src/Results.js
+++ src/Results.js
@@ -1,4 +1,5 @@
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 export default function Results(props) {
   console.log(props.results);
@@ -6,6 +7,13 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
+        {props.results.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index}>
+              <Phonetic phonetic={phonetic} />
+            </div>
+          );
+        })}
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
