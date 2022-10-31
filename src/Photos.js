08f2ef3e34c4e367fcf8279ed10fc7bb021f3be2
VicVeronique <107785778+VicVeronique@users.noreply.github.com>
2022-10-31 10:19:52 +0200

    rel="noopener noreferrer"

--- src/Photos.js
+++ src/Photos.js
@@ -10,7 +10,11 @@ export default function Photos(props) {
           {props.photos.map(function (photo, index) {
             return (
               <div className="col-4" key={index}>
-                <a href={photo.src.original} target="_blank">
+                <a
+                  href={photo.src.original}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <img src={photo.src.landscape} className="img-fluid" />
                 </a>
               </div>
